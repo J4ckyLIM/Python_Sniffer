749bf8f3ed0ea795b3a6ad8e608a67a0c96c83ea
Jacky LIM <jacky.lim@edu.itescia.fr>
2020-01-30 09:51:42 +0100

    First commit: Base code + Starting readMe

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Python_Sniffer
+
+## Installation
+```
+git clone https://github.com/J4ckyLIM/Python_Sniffer.git
+```
+## Requierements
+
+Python 2.7.16 or higher
+
+Check your Python version on your cmd, run:
+```
+python -V
+``` 
+
+Other requierement incoming.
+
+## Running the project
+
+No command available right now.
+
+### Notes
+
+The logs are shown in your command line tool.
+
---
+++ sniffer.py
@@ -0,0 +1,38 @@
+import socket
+import struct
+import time
+
+
+def decode(msg):
+    mac_dst = struct.unpack('!BBBBBB', msg[0:6])
+    mac_dst = ":".join([hex(part)[2:].zfill(2) for part in mac_dst])
+    mac_src = struct.unpack('!BBBBBB', msg[6:12])
+    mac_src = ":".join([hex(part)[2:].zfill(2) for part in mac_src])
+    ether_type = struct.unpack('!H', msg[12:14])
+    ether_type = ":".join([hex(part)[2:].zfill(4) for part in ether_type])
+    print(
+        f"{time.time()}: DST_MAC: {mac_dst} - "
+        f"SRC_MAC: {mac_src} - "
+        f"ETHER_TYPE: {ether_type} - "
+    )
+
+
+def main():
+    """Entry point"""
+    s = socket.socket(
+        socket.AF_PACKET,
+        socket.SOCK_RAW,
+        socket.htons(3),
+    )
+    s.bind(("enp0s3", 3))
+    try:
+        print("Sniffer started")
+        while True:
+            msg = s.recv(1024)P
+            decode(msg)
+    except KeyboardInterrupt:
+        print("Sniffer stopped")
+
+
+if __name__ == "__main__":
+    main()
