5339b566219be83a2231887276950fc30a665beb
Jacky LIM <jacky.lim@edu.itescia.fr>
2020-01-30 15:01:17 +0100

    ARP captured

--- sniffer.py
+++ sniffer.py
@@ -18,9 +18,22 @@ def decode(msg):
         ip_source = ".".join(str(part) for part in ip_source)
         ip_destination = struct.unpack('!4B', msg[30:34])
         ip_destination = ".".join(str(part) for part in ip_destination)
+        print(f"IP_SRC: {ip_source}")
+        print(f"IP_DST: {ip_destination}")
 
-    print(f"IP_SRC: {ip_source}")
-    print(f"IP_DST: {ip_destination}")
+    if ether_type == "0806":
+        protocole = struct.unpack('!2B', msg[16:18])
+        protocole = protocole[0]
+        sender_ha = struct.unpack('!6B', msg[23:29])
+        sender_ha = ".".join([hex(part)[2:].zfill(2) for part in sender_ha])
+        sender_ia = struct.unpack('!4B', msg[29:33])
+        tha = struct.unpack('!6B', msg[33:39])
+        tha = ".".join([hex(part)[2:].zfill(2) for part in tha])
+        tia = struct.unpack('!3B', msg[39:42])
+        print(f"Sender_HA: {sender_ha}")
+        print(f"Sender_IA: {sender_ia}")
+        print(f"TARGET HA: {tha}")
+        print(f"TARGET IA: {tia}")
 
     print(
         f"{time.time()}: DST_MAC: {mac_dst} - "
