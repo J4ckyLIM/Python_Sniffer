9d256fd0e20bf1d604a3efbd0c202f919f4823fa
Jacky LIM <jacky.lim@edu.itescia.fr>
2020-01-30 13:46:08 +0100

    Ipv4 decoder

--- sniffer.py
+++ sniffer.py
@@ -10,10 +10,23 @@ def decode(msg):
     mac_src = ":".join([hex(part)[2:].zfill(2) for part in mac_src])
     ether_type = struct.unpack('!H', msg[12:14])
     ether_type = ":".join([hex(part)[2:].zfill(4) for part in ether_type])
+
+    if ether_type == "0800":
+        protocole = struct.unpack('!BB', msg[23:25])
+        protocole = ":".join([hex(part)[2:].zfill(2) for part in protocole])
+        ip_source = struct.unpack('!BBBB', msg[26:30])
+        ip_source = ".".join(str(part) for part in ip_source)
+        ip_destination = struct.unpack('!4B', msg[30:34])
+        ip_destination = ".".join(str(part) for part in ip_destination)
+
+    print(f"IP_SRC: {ip_source}")
+    print(f"IP_DST: {ip_destination}")
+
     print(
         f"{time.time()}: DST_MAC: {mac_dst} - "
         f"SRC_MAC: {mac_src} - "
         f"ETHER_TYPE: {ether_type} - "
+        f"PROTOCOLE: {protocole}"
     )
 
 
